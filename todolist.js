3d695281844d05c3bd0a77f8484df4c4a91337bb
linjiang82 <linjiang82@gmail.com>
2016-08-24 22:08:14 +1000

    solve the first todo item being added to file by using php fwrite() and ajax post.

--- todolist.js
+++ todolist.js
@@ -1,3 +1,20 @@
 window.onload = function(){
+	function saveTofile(event){
+		var text="text=" + this.value;
+		var xhr= new XMLHttpRequest();
+		xhr.onreadystatechange = function(){
+	if(xhr.readyState === XMLHttpRequest.DONE && xhr.status ===200)
+	{
 
+	}
+		}
+		xhr.open('POST','writefile.php',true);
+		xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+		xhr.send(text);
+	}
+var items=document.getElementsByClassName("items");
+for(i=0;i<items.length;i++){
+//console.log(items[i].value);
+items[i].addEventListener('keyup',saveTofile)//when sth entered, save to a file and create another line for a new item.
+}
 };
